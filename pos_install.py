cb5bdb8fa0df6be55be25c1ad1902a5ddfc4a8ae
André Minoro Fusioka <minorofusioka@hotmail.com>
2016-11-28 20:53:10 -0200

    instalação de pacotes multimidia

--- pos_install.py
+++ pos_install.py
@@ -1,4 +1,38 @@
-import subprocess
-import os
+# -*- coding:utf-8 -*-
 
-subprocess.run(['apt-get', 'install', '-y', 'git'])
+import subprocess
+
+programs_dev = ['python3', 'git', 'openjdk-9-jdk', 'python-software-properties',
+                'php7.0', 'php7.0-fpm', 'php7.0-mysql', 'libapache2-mod-php7.0', 'composer']
+
+programs_dev_research = ['python3-scipy', 'octave', 'r-base', 'python-matplotlib', 'text_live',
+                         'texlive-latex-extra', 'texlive-lang-portuguese', 'texlive-math-extra']
+
+programs_media = ['vlc', 'amarok' ]
+programs_utils = ['wine']
+
+
+def install_media(**kwargs):
+
+    output = ''
+    for program in programs_media:
+        try:
+            print('Instalando: '+program)
+            install_process = subprocess.Popen(['apt-get', 'install', '-y', program],
+                                               stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            out, _ = install_process.communicate()
+            output += out.decode('utf-8')
+            print('Instalado: '+program)
+
+        except Exception:
+            print('Erro ao installar o programa')
+            output, _ = install_process.communicate()
+
+
+    print(output)
+
+def __main__(**kwargs):
+    install_all()
+
+if __name__ == '__main__':
+    __main__()
